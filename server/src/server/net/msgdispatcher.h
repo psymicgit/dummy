df3e7187a83103f83bdaf984fb0e7ebccbb8d59f
psymicgit <wuzili1234@gmail.com>
2015-09-08 17:26:38 +0800

    调整消息派发机制

--- server/src/server/net/msgdispatcher.h
+++ server/src/server/net/msgdispatcher.h
@@ -15,34 +15,77 @@
 #include "tool/timetool.h"
 #include "imsghandler.h"
 
+
+// 消息回调基类
+template <typename LinkType>
+class Callback
+{
+public:
+	virtual void onMessage(LinkType&, const char* data, int len, Timestamp) const = 0;
+};
+
+// 针对某个具体消息类别的回调
+template <typename LinkType, typename MessageType>
+class CallbackT : public Callback<LinkType>
+{
+public:
+	typedef void (*ProtobufMessageCallback)(LinkType*, MessageType* message, Timestamp);
+
+	CallbackT(const ProtobufMessageCallback &callback)
+		: m_callback(callback)
+	{
+	}
+
+	virtual void onMessage(LinkType& link, const char* data, int len, Timestamp receiveTime) const
+	{
+		MessageType t;
+
+		bool ok = t.ParseFromArray(data, len);
+		if (!ok) {
+			LOG_ERROR << "decode packet <" << t.GetTypeName() << "> = <" << t.SerializeAsString() << "> error, data len = " << len;
+			// LOG_INFO << "decode packet <" << msgtool::getMsgString(*t) << "> error";
+			return;
+		}
+
+		m_callback(&link, &t, receiveTime);
+	}
+
+private:
+	// 消息回调函数
+	ProtobufMessageCallback m_callback;
+};
+
 // 消息派发器：负责把消息包派发给合适的消息处理器
 // 注意：<消息派发器>管理多个<消息处理器>，而每个<消息处理器>内又注册了不同的<消息回调函数>
 template <typename LinkType>
 class MsgDispatcher
 {
 private:
+	typedef tr1::unordered_map<int, Callback<LinkType>*> CallbackMap;
 	typedef tr1::unordered_map<int, IMsgHandler<LinkType>*> MsgHandlerMap;
 	typedef std::vector<IMsgHandler<LinkType>*> MsgHandlerVec;
 
 public:
-	// 这里register会与原有命名冲突，所以用Register
-	void Register(int msgId, IMsgHandler<LinkType> *msgHandler)
+	template <typename MessageType>
+	void registerMsg(int msgId, CallbackT<LinkType, MessageType> *pd)
 	{
-		m_msgMap[msgId] = msgHandler;
+		m_callbackMap[msgId] = pd;
 	}
 
 	// 派发消息包
 	void dispatch(LinkType& link, int msgId, const char* data, int len, Timestamp receiveTime)
 	{
 		// 首先根据消息id找到预先注册好的对应的消息包处理器
-		typename MsgHandlerMap::const_iterator itr = m_msgMap.find(msgId);
-		if (itr == m_msgMap.end()) {
+		// 寻找到对应的消息回调函数
+		typename CallbackMap::const_iterator it = m_callbackMap.find(msgId);
+		if (it == m_callbackMap.end()) {
+			LOG_DEBUG << "Discard msg[id=" << msgId << "]";
 			return;
 		}
 
-		// 再由该消息处理器处理对应的消息包
-		IMsgHandler<LinkType> *msgMgr = itr->second;
-		msgMgr->onMessage(link, msgId, data, len, receiveTime);
+		// 处理消息包
+		Callback<LinkType> *callback = it->second;
+		callback->onMessage(link, data, len, receiveTime);
 	}
 
 	void addMsgHandler(IMsgHandler<LinkType> *msgHandler)
@@ -52,19 +95,12 @@ public:
 
 	void clear()
 	{
-		// LOG_INFO << "clear";
 
-		for (size_t i = 0; i < m_msgHandlerVec.size(); i++) {
-			IMsgHandler<LinkType> *msgHandler = m_msgHandlerVec[i];
-			msgHandler->clear();
-			delete msgHandler;
-		}
-
-		m_msgHandlerVec.clear();
-		m_msgMap.clear();
 	}
 
 private:
+	// 消息回调map
+	CallbackMap m_callbackMap;
 	MsgHandlerMap m_msgMap;
 	MsgHandlerVec m_msgHandlerVec;;
 };
--- server/src/server/net/msghandler.h
+++ server/src/server/net/msghandler.h
@@ -19,45 +19,6 @@
 
 class Link;
 
-// 消息回调基类
-template <typename LinkType>
-class Callback
-{
-public:
-	virtual void onMessage(LinkType&, const char* data, int len, Timestamp) const = 0;
-};
-
-// 针对某个具体消息类别的回调
-template <typename LinkType, typename MessageType>
-class CallbackT : public Callback<LinkType>
-{
-public:
-	typedef void (*ProtobufMessageCallback)(LinkType*, MessageType* message, Timestamp);
-
-	CallbackT(const ProtobufMessageCallback &callback)
-		: m_callback(callback)
-	{
-	}
-
-	virtual void onMessage(LinkType& link, const char* data, int len, Timestamp receiveTime) const
-	{
-		MessageType t;
-
-		bool ok = t.ParseFromArray(data, len);
-		if (!ok) {
-			LOG_ERROR << "decode packet <" << t.GetTypeName() << "> = <" << t.SerializeAsString() << "> error, data len = " << len;
-			// LOG_INFO << "decode packet <" << msgtool::getMsgString(*t) << "> error";
-			return;
-		}
-
-		m_callback(&link, &t, receiveTime);
-	}
-
-private:
-	// 消息回调函数
-	ProtobufMessageCallback m_callback;
-};
-
 // 消息处理器
 // （这里之所以提取出IMsgHandler接口是为了防止模板文件互相包含导致编译不过）
 // （LinkType代表从连接实例身上得到的message，比如代表客户端连接的Client、代表底层网络连接的Link等）
@@ -76,16 +37,7 @@ public:
 	// 处理接收到的数据
 	void onMessage(LinkType& link, int msgId, const char* data, int len, Timestamp receiveTime) const
 	{
-		// 寻找到对应的消息回调函数
-		typename CallbackMap::const_iterator it = m_callbackMap.find(msgId);
-		if (it == m_callbackMap.end()) {
-			LOG_DEBUG << "Discard msg[id=" << msgId << "]";
-			return;
-		}
 
-		// 处理消息包
-		Callback<LinkType> *callback = it->second;
-		callback->onMessage(link, data, len, receiveTime);
 	}
 
 	// 对某个消息id注册对应的回调函数
@@ -95,7 +47,7 @@ public:
 		CallbackT<LinkType, MessageType> *pd = new CallbackT<LinkType, MessageType>(callback);
 		m_callbackMap[msgId] = pd;
 
-		m_msgDispacher->Register(msgId, this);
+		m_msgDispacher->registerMsg(msgId, pd);
 	}
 
 	void discardMessage(Link& link, const Message& message, Timestamp receiveTime) const
